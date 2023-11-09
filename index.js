256890e923dec3326bddbc77460d0384c6f93c4b
aechlaenm <teeraphatalt@gmail.com>
2023-11-09 20:25:17 +0700

    2

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const noblox = require('noblox.js');
 const app = express();
 const PORT = 3000;
+
+const cookie = process.env.COOKIE;
+const groupid = process.env.GROUPID;
+
+async function startApp() {
+    await noblox.setCookie(cookie);
+    let currentUser = await noblox.getCurrentUser(); // Get information about the current user
+    console.log(`Logged in as ${currentUser.UserName} (${currentUser.UserID})`); // Log the current user's username and ID to the console
+}
+
+startApp();
 
 app.listen(PORT, () => {
     console.log(`API Listening on PORT ${PORT}`);
